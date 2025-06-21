1a59cb0023dca6d66aaa88c5b25c32d8280ddb8d
tharuz23 <nethminit2003@gmail.com>
2025-06-21 15:49:48 +0530

    Create delete_item.php

---
+++ delete_item.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header('Location: admin_login.php');
+    exit();
+}
+
+include 'db_connect.php';
+
+if (!isset($_GET['id'])) {
+    die('ID not specified.');
+}
+
+$id = intval($_GET['id']);
+
+
+$stmt = $conn->prepare("DELETE FROM icecream_items WHERE id = ?");
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    $stmt->close();
+    header('Location: admin_panel.php?msg=deleted');
+    exit();
+} else {
+    die('Error deleting item: ' . $conn->error);
+}
