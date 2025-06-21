98798b34d7d373827b8f55087bb5e91ba6d3d4c3
tharuz23 <nethminit2003@gmail.com>
2025-06-21 15:44:47 +0530

    Create admin_panel.php

---
+++ admin_panel.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header('Location: admin_login.php');
+    exit();
+}
+
+
+include 'db_connect.php'; 
+
+
+$sql = "SELECT * FROM icecream_items"; 
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Panel - ScoopNest</title>
+</head>
+<body>
+    <h2>Welcome, Admin!</h2>
+    <a href="logout.php">Logout</a>
+    <h3>Ice Cream Items</h3>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Flavor</th>
+            <th>Price</th>
+            <th>Actions</th>
+        </tr>
+        <?php while($row = $result->fetch_assoc()) { ?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo htmlspecialchars($row['name']); ?></td>
+            <td><?php echo htmlspecialchars($row['flavor']); ?></td>
+            <td><?php echo $row['price']; ?></td>
+            <td>
+                <a href="edit_item.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                <a href="delete_item.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Delete this item?');">Delete</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
+    <br>
+    <a href="add_item.php">Add New Ice Cream Item</a>
+</body>
+</html>
