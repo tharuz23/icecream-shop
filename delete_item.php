ff8b65115cad253f871962fe4b98b7def5698a68
tharuz23 <nethminit2003@gmail.com>
2025-06-21 18:32:51 +0530

    Update delete_item.php

--- delete_item.php
+++ delete_item.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     exit();
 }
 
-include 'db_connect.php';
+include 'db.php';
 
 if (!isset($_GET['id'])) {
     die('ID not specified.');
